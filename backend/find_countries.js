24ec8686c01577f66418b0d806ee0a6dad34f0e3
pszkudlar <94530509+pszkudlar@users.noreply.github.com>
2023-07-23 10:51:16 +0200

    New_repo

--- backend/find_countries.js
+++ backend/find_countries.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const { stringify } = require('querystring')
 
 const countries_file = fs.readFileSync("C:/Users/Piotr Szkudlarek/Desktop/Osobiste/miasta/backend/countries.json", 'utf8')
 let countriesArr =  JSON.parse(countries_file)
@@ -7,27 +6,22 @@ console.log(typeof(countriesArr))
 function FilterIt(substring){
     let stringifiedSubsting = String(substring)
     let filteredArray = []
-    debugger
-    console.log(stringifiedSubsting)
     let typeOfSubstring = typeof(stringifiedSubsting)
-    console.log(typeOfSubstring)
-    debugger
-    let capitalizedSubstring = substrstringifiedSubstinging.toUpperCase()
-    debugger
+    let capitalizedSubstring = stringifiedSubsting.toUpperCase()
     for (let i = 0; i<251;i++){
         let checkedCountry = countriesArr[i]
         let countryName = checkedCountry.name
         let countryCode = checkedCountry.alpha_2
         let capitalizedName = countryName.toUpperCase()
         if (capitalizedName.includes(capitalizedSubstring)) {
+            console.log(countryName)
             let flagToAdd = countryCode.toLowerCase()+'.png'
             let objToPush = {"name": countryName, "flag": flagToAdd}
-            filteredArray.push(objToPush) 
+            filteredArray.push(objToPush)
         }
     }
     const returnedArray = JSON.stringify(filteredArray)
-    console.log(returnedArray)
-    return returnedArray
+    //return returnedArray
 }
-FilterIt("st")
+FilterIt("str")
 //module.exports = FilterIt
